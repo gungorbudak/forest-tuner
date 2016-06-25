60689ca749b15e375e51fa2996cc7f5457e47884
Gungor Budak <gngrbdk@gmail.com>
2016-06-26 00:23:34 +0300

    several fixes for getting the best solution, checking if dir and file arguments exist

--- forest-tuner.py
+++ forest-tuner.py
@@ -77,28 +77,31 @@ def get_forest_feature(F):
 
 
 def get_best_solution(solutions):
-    features = [get_forest_feature(sln['forest']) for sln in solutions]
-    return solutions[features.index(min(features))]
+    min_feature = None
+    best_solution = {}
+    for solution in solutions:
+        feature = get_forest_feature(solution['forest'])
+        if min_feature == None:
+            min_feature = feature
+            best_solution = solution
+        else:
+            if feature < min_feature:
+                min_feature = feature
+                best_solution = solution
+    return best_solution
 
 
 def run_forest_run(outputs_dir, forest_path, msgsteiner_path,
         prizes_file, edges_file, config_file, label):
     # create a command for running forest
     command = [
-        'python',
-        forest_path,
-        '--msgpath',
-        msgsteiner_path,
-        '-p',
-        prizes_file,
-        '-e',
-        edges_file,
-        '-c',
-        config_file,
-        '--outpath',
-        outputs_dir,
-        '--outlabel',
-        label
+        'python', forest_path,
+        '--msgpath', msgsteiner_path,
+        '-p', prizes_file,
+        '-e', edges_file,
+        '-c', config_file,
+        '--outpath', outputs_dir,
+        '--outlabel', label
     ]
 
     # run the command
@@ -113,11 +116,25 @@ def run_forest_run(outputs_dir, forest_path, msgsteiner_path,
     make_log_file(outputs_dir, stdin, stdout, label)
 
     # delete all output files except optimal forest
-    os.remove(os.path.join(outputs_dir, label + '_augmentedForest.sif'))
-    os.remove(os.path.join(outputs_dir, label + '_dummyForest.sif'))
-    os.remove(os.path.join(outputs_dir, label + '_edgeattributes.tsv'))
-    os.remove(os.path.join(outputs_dir, label + '_nodeattributes.tsv'))
-    os.remove(os.path.join(outputs_dir, label + '_info.txt'))
+    augmented_forest_file = os.path.join(outputs_dir, label + '_augmentedForest.sif')
+    if os.path.exists(augmented_forest_file):
+        os.remove(augmented_forest_file)
+
+    dummy_forest_file = os.path.join(outputs_dir, label + '_dummyForest.sif')
+    if os.path.exists(dummy_forest_file):
+        os.remove(dummy_forest_file)
+
+    edge_attributes_file = os.path.join(outputs_dir, label + '_edgeattributes.tsv')
+    if os.path.exists(edge_attributes_file):
+        os.remove(edge_attributes_file)
+
+    node_attributes_file = os.path.join(outputs_dir, label + '_nodeattributes.tsv')
+    if os.path.exists(node_attributes_file):
+        os.remove(node_attributes_file)
+
+    info_file = os.path.join(outputs_dir, label + '_info.txt')
+    if os.path.exists(info_file):
+        os.remove(info_file)
 
     # return the optimal forest obtained for testing
     return os.path.join(outputs_dir, label + '_optimalForest.sif')
@@ -151,11 +168,31 @@ def main():
         help='Size of w and b values to tune for')
     args = parser.parse_args()
 
+    if not os.path.isdir(args.workingDir):
+        print 'The working directory {} is NOT a valid directory'.format(args.workingDir)
+        raise SystemExit
+
+    if not os.path.isfile(args.forestPath):
+        print 'The forest path {} is NOT a valid file path'.format(args.forestPath)
+        raise SystemExit
+
+    if not os.path.isfile(args.msgsteinerPath):
+        print 'The msgsteiner path {} is NOT a valid file path'.format(args.msgsteinerPath)
+        raise SystemExit
+
+    if not os.path.isfile(args.prizePath):
+        print 'The prize path {} is NOT a valid file path'.format(args.prizePath)
+        raise SystemExit
+
+    if not os.path.isfile(args.edgePath):
+        print 'The edge path {} is NOT a valid file path'.format(args.edgePath)
+        raise SystemExit
+
     # get configurations based on user input
     configs = get_configs(
-                {'start': args.wStart, 'end': args.wEnd},
-                {'start': args.bStart, 'end': args.bEnd},
-                args.size)
+        {'start': args.wStart, 'end': args.wEnd},
+        {'start': args.bStart, 'end': args.bEnd},
+        args.size)
 
     # directory for storing outputs
     outputs_dir = os.path.join(args.workingDir, 'outputs')
@@ -164,24 +201,34 @@ def main():
     print 'Running forest for', args.prizePath
 
     # list to collect optimal forests obtained
-    solutions = []
+    results = []
 
     # run forest for each configuration obtained based on user input
     for config in configs:
-        print ', '.join([k + ': ' + str(v) for k, v in config.iteritems()])
         label = get_label(args.prizePath, config['w'], config['b'])
         config_file = make_config_file(args.workingDir, config, label)
         forest_file = run_forest_run(outputs_dir, args.forestPath,
             args.msgsteinerPath, args.prizePath, args.edgePath, config_file, label)
-        solutions.append({
+        results.append({
             'config': config,
-            'forest': get_forest(forest_file)
+            'forest_file': forest_file
             })
+
+    # collect the solutions with at least one edge
+    solutions = []
+    for result in results:
+        F = get_forest(result['forest_file'])
+        if F.number_of_edges() > 0:
+            solutions.append({
+                'config': result['config'],
+                'forest': F
+                })
 
     # get the best solution based on number of trees
     # and avg size of trees in the optimal forests
     # and write the best solution to a text file
     solution = get_best_solution(solutions)
+    label = get_label(args.prizePath, solution['config']['w'], solution['config']['b'])
     forest_file = make_forest_file(outputs_dir, solution['forest'], label)
     print 'Best config', ', '.join([
         k + ': ' + str(v) for k, v in solution['config'].iteritems()])
