8d2d2f586c9b107015591deb5eed7e4b0e3edfef
Gungor Budak <gngrbdk@gmail.com>
2016-06-27 17:03:27 +0300

    filtering solutions based on number of terminal nodes

--- README.md
+++ README.md
@@ -12,34 +12,40 @@ Download `forest-tuner.py` script from this repository and move to your working 
 
 ## Run
 
+### Example
+
+    python forest-tuner.py --workingDir /home/gungor/projects/cptac --forestPath /home/gungor/softwares/OmicsIntegrator-0.2.0/scripts/forest.py --msgsteinerPath /home/gungor/softwares/msgsteiner-1.1/msgsteiner --edgePath /home/gungor/projects/cptac/PSICQUIC_UPDATED_05162013.txt --prizePath /home/gungor/projects/cptac/prizes/ovarian_S1T60.txt --wStart 1.0 --wEnd 10.0 --bStart 1.0 --bEnd 10.0 --size 10
+
+
 ### Getting help
 
-    usage: forest-tuner.py [-h] [--workingDir WORKINGDIR] --forestPath FORESTPATH
-                       --msgsteinerPath MSGSTEINERPATH --prizePath PRIZEPATH
-                       --edgePath EDGEPATH [--wStart WSTART] [--wEnd WEND]
-                       [--bStart BSTART] [--bEnd BEND] [--size SIZE]
+    usage: forest-tuner.py [-h] [--workingDir DIR] --forestPath FILE
+                           --msgsteinerPath FILE --prizePath FILE --edgePath FILE
+                           [--wStart DECIMAL] [--wEnd DECIMAL] [--bStart DECIMAL]
+                           [--bEnd DECIMAL] [--size INTEGER] [--minEdges INTEGER]
+                           [--minTerminalNodes INTEGER]
 
     Prize-collecting Steiner Forest algorithm parameter tuner for w and b
     parameters
 
     optional arguments:
-    -h, --help            show this help message and exit
-    --workingDir WORKINGDIR
-                        Working directory for configs, logs and outputs
-                        (default: .)
-    --forestPath FORESTPATH
-                        Absolute path to forest Python script (default: None)
-    --msgsteinerPath MSGSTEINERPATH
-                        Absolute path to msgsteiner executable (default: None)
-    --prizePath PRIZEPATH
-                        Absolute path to prize file (default: None)
-    --edgePath EDGEPATH   Absolute path to edge file (default: None)
-    --wStart WSTART       Starting value for w (default: 1.0)
-    --wEnd WEND           Ending value for w (default: 10.0)
-    --bStart BSTART       Starting value for b (default: 1.0)
-    --bEnd BEND           Ending value for b (default: 10.0)
-    --size SIZE           Size of w and b values to tune for (default: 10)
-
-### Example
-
-    python forest-tuner.py --workingDir /home/gungor/projects/cptac --forestPath /home/gungor/softwares/OmicsIntegrator-0.2.0/scripts/forest.py --msgsteinerPath /home/gungor/softwares/msgsteiner-1.1/msgsteiner --edgePath /home/gungor/projects/cptac/PSICQUIC_UPDATED_05162013.txt --prizePath /home/gungor/projects/cptac/prizes/ovarian_S1T60.txt --wStart 1.0 --wEnd 10.0 --bStart 1.0 --bEnd 10.0 --size 10
+      -h, --help            show this help message and exit
+      --workingDir DIR      Working directory for configs and outputs (default: .)
+      --forestPath FILE     (required) Absolute path to forest Python script
+                            (default: None)
+      --msgsteinerPath FILE
+                            (required) Absolute path to msgsteiner executable
+                            (default: None)
+      --prizePath FILE      (required) Absolute path to prize file (default: None)
+      --edgePath FILE       (required) Absolute path to edge file (default: None)
+      --wStart DECIMAL      Starting value for w (default: 1.0)
+      --wEnd DECIMAL        Ending value for w (default: 10.0)
+      --bStart DECIMAL      Starting value for b (default: 1.0)
+      --bEnd DECIMAL        Ending value for b (default: 10.0)
+      --size INTEGER        Size of w and b values to tune for (default: 10)
+      --minEdges INTEGER    Minimum number of edges in optimal forests to consider
+                            as a solution (default: 1)
+      --minTerminalNodes INTEGER
+                            Minimum percentage of nodes in optimal forests
+                            overlapping with terminal nodes in prize file to
+                            consider as a solution (default: 60)
--- forest-tuner.py
+++ forest-tuner.py
@@ -31,17 +31,18 @@ def make_log_file(outputs_dir, stdout, stderr, label):
     return
 
 
-def make_forest_file(outputs_dir, forest, label):
-    forest_file = os.path.join(outputs_dir, label + '_bestOptimalForest.tsv')
-    with open(forest_file, 'w') as f:
-        for edge in forest.edges():
+def make_network_file(outputs_dir, N, label, sif = True):
+    extension = '.sif' if sif else '.tsv'
+    network_file = os.path.join(outputs_dir, label + extension)
+    with open(network_file, 'w') as f:
+        for edge in N.edges():
             f.write(edge[0] + '\t' + edge[1] + '\n')
-    return forest_file
+    return network_file
 
 
-def get_label(prize_file, w, b):
+def get_label(prize_path, w, b):
     label = ''.join([
-        os.path.splitext(os.path.basename(prize_file))[0], '_',
+        os.path.splitext(os.path.basename(prize_path))[0], '_',
         'W', str(w), 'B', str(b)])
     return label
 
@@ -53,7 +54,9 @@ def get_configs(w, b, size):
     for i in xrange(size):
         for j in xrange(size):
             configs.append({
+                # 'w': round(ws[i], 2),
                 'w': ws[i],
+                # 'b': round(bs[j], 2),
                 'b': bs[j],
                 'D': 10,
                 'mu': 0.01
@@ -61,13 +64,23 @@ def get_configs(w, b, size):
     return configs
 
 
-def get_forest(forest_file):
-    F = nx.Graph()
-    with open(forest_file) as rows:
+def get_network(network_file, sif = True):
+    N = nx.Graph()
+    target_index = 2 if sif else 1
+    with open(network_file) as rows:
         for row in rows:
             cols = row.strip().split()
-            F.add_edge(cols[0], cols[2])
-    return F
+            N.add_edge(cols[0], cols[target_index])
+    return N
+
+
+def get_terminal_nodes(prize_path):
+    nodes = set()
+    with open(prize_path) as rows:
+        for row in rows:
+            cols = row.strip().split()
+            nodes.add(cols[0])
+    return nodes
 
 
 def get_forest_feature(F):
@@ -80,7 +93,7 @@ def get_best_solution(solutions):
     min_feature = None
     best_solution = {}
     for solution in solutions:
-        feature = get_forest_feature(solution['forest'])
+        feature = get_forest_feature(solution['F'])
         if min_feature == None:
             min_feature = feature
             best_solution = solution
@@ -92,15 +105,16 @@ def get_best_solution(solutions):
 
 
 def run_forest_run(outputs_dir, forest_path, msgsteiner_path,
-        prizes_file, edges_file, config_file, label):
-    optimal_forest_file = os.path.join(outputs_dir, label + '_optimalForest.sif')
+        prize_path, edge_path, config_file, label):
+    optimal_forest_file = os.path.join(outputs_dir,
+        label + '_optimalForest.sif')
     if not os.path.exists(optimal_forest_file):
         # create a command for running forest
         command = [
             'python', forest_path,
             '--msgpath', msgsteiner_path,
-            '-p', prizes_file,
-            '-e', edges_file,
+            '-p', prize_path,
+            '-e', edge_path,
             '-c', config_file,
             '--outpath', outputs_dir,
             '--outlabel', label
@@ -118,23 +132,28 @@ def run_forest_run(outputs_dir, forest_path, msgsteiner_path,
         make_log_file(outputs_dir, stdout, stderr, label)
 
         # delete all output files except optimal forest
-        augmented_forest_file = os.path.join(outputs_dir, label + '_augmentedForest.sif')
+        augmented_forest_file = os.path.join(outputs_dir,
+            label + '_augmentedForest.sif')
         if os.path.exists(augmented_forest_file):
             os.remove(augmented_forest_file)
 
-        dummy_forest_file = os.path.join(outputs_dir, label + '_dummyForest.sif')
+        dummy_forest_file = os.path.join(outputs_dir,
+            label + '_dummyForest.sif')
         if os.path.exists(dummy_forest_file):
             os.remove(dummy_forest_file)
 
-        edge_attributes_file = os.path.join(outputs_dir, label + '_edgeattributes.tsv')
+        edge_attributes_file = os.path.join(outputs_dir,
+            label + '_edgeattributes.tsv')
         if os.path.exists(edge_attributes_file):
             os.remove(edge_attributes_file)
 
-        node_attributes_file = os.path.join(outputs_dir, label + '_nodeattributes.tsv')
+        node_attributes_file = os.path.join(outputs_dir,
+            label + '_nodeattributes.tsv')
         if os.path.exists(node_attributes_file):
             os.remove(node_attributes_file)
 
-        info_file = os.path.join(outputs_dir, label + '_info.txt')
+        info_file = os.path.join(outputs_dir,
+            label + '_info.txt')
         if os.path.exists(info_file):
             os.remove(info_file)
 
@@ -148,46 +167,65 @@ def main():
         description='Prize-collecting Steiner Forest algorithm\
                      parameter tuner for w and b parameters',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--workingDir', default='.',
+    parser.add_argument('--workingDir', metavar='DIR', default='.',
         help='Working directory for configs and outputs')
-    parser.add_argument('--forestPath', required=True,
-        help='Absolute path to forest Python script')
-    parser.add_argument('--msgsteinerPath', required=True,
-        help='Absolute path to msgsteiner executable')
-    parser.add_argument('--prizePath', required=True,
-        help='Absolute path to prize file')
-    parser.add_argument('--edgePath', required=True,
-        help='Absolute path to edge file')
-    parser.add_argument('--wStart', type=float, default=1.0,
+    parser.add_argument('--forestPath', metavar='FILE', required=True,
+        help='(required) Absolute path to forest Python script')
+    parser.add_argument('--msgsteinerPath', metavar='FILE', required=True,
+        help='(required) Absolute path to msgsteiner executable')
+    parser.add_argument('--prizePath', metavar='FILE', required=True,
+        help='(required) Absolute path to prize file')
+    parser.add_argument('--edgePath', metavar='FILE', required=True,
+        help='(required) Absolute path to edge file')
+    parser.add_argument('--wStart', metavar='DECIMAL',
+        type=float, default=1.0,
         help='Starting value for w')
-    parser.add_argument('--wEnd', type=float, default=10.0,
+    parser.add_argument('--wEnd', metavar='DECIMAL',
+        type=float, default=10.0,
         help='Ending value for w')
-    parser.add_argument('--bStart', type=float, default=1.0,
+    parser.add_argument('--bStart', metavar='DECIMAL',
+        type=float, default=1.0,
         help='Starting value for b')
-    parser.add_argument('--bEnd', type=float, default=10.0,
+    parser.add_argument('--bEnd', metavar='DECIMAL',
+        type=float, default=10.0,
         help='Ending value for b')
-    parser.add_argument('--size', type=int, default=10,
+    parser.add_argument('--size', metavar='INTEGER',
+        type=int, default=10,
         help='Size of w and b values to tune for')
+    parser.add_argument('--minEdges', metavar='INTEGER',
+        type=int, default=1,
+        help='Minimum number of edges in optimal forests\
+              to consider as a solution')
+    parser.add_argument('--minTerminalNodes', metavar='INTEGER',
+        type=int, default=60,
+        help='Minimum percentage of nodes in optimal forests\
+              overlapping with terminal nodes in prize file\
+              to consider as a solution')
     args = parser.parse_args()
 
     if not os.path.isdir(args.workingDir):
-        print 'The working directory {} is NOT a valid directory'.format(args.workingDir)
+        print 'The working directory {} is NOT a valid directory'.format(
+            args.workingDir)
         raise SystemExit
 
     if not os.path.isfile(args.forestPath):
-        print 'The forest path {} is NOT a valid file path'.format(args.forestPath)
+        print 'The forest path {} is NOT a valid file path'.format(
+            args.forestPath)
         raise SystemExit
 
     if not os.path.isfile(args.msgsteinerPath):
-        print 'The msgsteiner path {} is NOT a valid file path'.format(args.msgsteinerPath)
+        print 'The msgsteiner path {} is NOT a valid file path'.format(
+            args.msgsteinerPath)
         raise SystemExit
 
     if not os.path.isfile(args.prizePath):
-        print 'The prize path {} is NOT a valid file path'.format(args.prizePath)
+        print 'The prize path {} is NOT a valid file path'.format(
+            args.prizePath)
         raise SystemExit
 
     if not os.path.isfile(args.edgePath):
-        print 'The edge path {} is NOT a valid file path'.format(args.edgePath)
+        print 'The edge path {} is NOT a valid file path'.format(
+            args.edgePath)
         raise SystemExit
 
     # get configurations based on user input
@@ -200,7 +238,7 @@ def main():
     outputs_dir = os.path.join(args.workingDir, 'outputs')
     if not os.path.exists(outputs_dir):
         os.mkdir(outputs_dir)
-    print 'Running forest for', args.prizePath
+    print 'Running forest for {}'.format(args.prizePath)
 
     # list to collect optimal forests obtained
     results = []
@@ -210,31 +248,44 @@ def main():
         label = get_label(args.prizePath, config['w'], config['b'])
         config_file = make_config_file(args.workingDir, config, label)
         forest_file = run_forest_run(outputs_dir, args.forestPath,
-            args.msgsteinerPath, args.prizePath, args.edgePath, config_file, label)
+            args.msgsteinerPath, args.prizePath, args.edgePath,
+            config_file, label)
         results.append({
             'config': config,
             'forest_file': forest_file
             })
 
-    # collect the solutions with more than one edge
+    # read terminal nodes from the prize file
+    terminal_nodes = get_terminal_nodes(args.prizePath)
+    terminal_nodes_size = float(len(terminal_nodes))
+
+    # collect the solutions under certain conditions
     solutions = []
     for result in results:
-        F = get_forest(result['forest_file'])
-        if F.number_of_edges() > 1:
-            solutions.append({
-                'config': result['config'],
-                'forest': F
-                })
+        F = get_network(result['forest_file'])
+        # don't include forests with less than given number of edges
+        if args.minEdges < F.number_of_edges():
+            # only if more than 60 percent of terminal nodes
+            # are present in the solution
+            overlap = 100 * (len(terminal_nodes.intersection(F.nodes())) /
+                terminal_nodes_size)
+            if overlap > args.minTerminalNodes:
+                solutions.append({
+                    'config': result['config'],
+                    'F': F
+                    })
 
     # get the best solution based on number of trees
-    # and avg size of trees in the optimal forests
+    # and average size of trees in the optimal forests
     # and write the best solution to a text file
     solution = get_best_solution(solutions)
-    label = get_label(args.prizePath, solution['config']['w'], solution['config']['b'])
-    forest_file = make_forest_file(outputs_dir, solution['forest'], label)
-    print 'Best config', ', '.join([
-        k + ': ' + str(v) for k, v in solution['config'].iteritems()])
-    print 'Best solution written to', forest_file
+    label = get_label(args.prizePath,
+        solution['config']['w'], solution['config']['b'])
+    forest_file = make_network_file(outputs_dir, solution['F'],
+        label + '_bestOptimalForest', sif = False)
+    print 'Best config. found to be {}'.format(', '.join([
+        k + ': ' + str(v) for k, v in solution['config'].iteritems()]))
+    print 'Best solution written to {}'.format(forest_file)
 
 
 if __name__ == '__main__':
